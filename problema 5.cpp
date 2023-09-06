13c80291962f5d344de074cf7f5371649846b662
sdmenos1 <130485609+sdmenos1@users.noreply.github.com>
2023-09-06 09:52:04 -0500

    Add files via upload

---
+++ problema 5.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+	for(int i=10;i<=98;i=i+2){
+		cout<<i<< endl;
+	}
+	return 0;
+}
---
+++ trabajo 6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+a=10;
+while(a<=98){
+   cout<<a<<" ";
+   a=a+2;
+}
+return 0;
+}
